b6865c5842c3ffc5e985e1be3b4faf98afa783f2
gvalencial81CR <gvalencial81@gmail.com>
2026-09-14 22:38:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import sqlite3
 import urllib.parse
 from datetime import date, datetime, time
 
-import libsql_experimental as libsql
+import libsql_client as libsql
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
 import streamlit as st
